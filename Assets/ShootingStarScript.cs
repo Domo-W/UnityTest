f6bb305253e7f572d8ac99a45316564ab1f2f9a9
Daniel Lin <dlin@scu.edu>
2024-05-13 00:28:12 -0700

    Shooting star updates
    Stars now point in the right direction when being spawned, just need to add functionality to do it automatically and to fix the weird issue where it overshoots and turns around again (look into making the look at function not in update)

--- Assets/ShootingStarScript.cs
+++ Assets/ShootingStarScript.cs
@@ -13,31 +13,37 @@ public class ShootingStarScript : MonoBehaviour
     private float z;
     private float timeToLive;
 
+    public GameObject Target;
+    private GameObject t;
     void Start()
     {
-        x = Random.Range(15.0f, 30.0f);
-        y = Random.Range(5.0f, 10.0f);
-        z = Random.Range(15.0f, 30.0f);
+        x = Random.Range(100.0f, 150.0f);
+        y = Random.Range(20.0f, 40.0f);
+        z = Random.Range(100.0f, 150.0f);
 
         timeToLive = Random.Range(1.5f, 2.0f);
 
         speed = Random.Range(1.0f, 2.0f);
 
+        t = Instantiate(Target, new Vector3(x, transform.position.y, z), Quaternion.identity);
         StartCoroutine(StartMetorite());
+        //StartCoroutine(PushMetorite());
 
-}
+    }
 
     // Update is called once per frame
     void Update()
     {
-     
+        transform.LookAt(t.transform);
     }
 
     private IEnumerator StartMetorite()
     {
         float timer = 0f;
-        Vector3 targetPosition = new Vector3(x, y, z);
+        Vector3 targetPosition = new Vector3(x, transform.position.y - y, z);
         Vector3 startingPosition = transform.position;
+
+        
         while(timer < timeToLive)
         {
             float t = Mathf.SmoothStep(0, 1, timer / timeToLive);
@@ -47,9 +53,23 @@ public class ShootingStarScript : MonoBehaviour
         }
         transform.position = targetPosition;
         // destroy here 
-        this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+        Destroy(t.gameObject);
     }
 
+    private IEnumerator PushMetorite()
+    {
+        float timer = 0f;
+        while (timer < timeToLive)
+        {
+            transform.TransformDirection(Vector3.forward * 50);
+            yield return null; 
+            timer += Time.deltaTime;
+        }
+
+
+      
+    }
 
     // you have a star spawning manager, that spawns them in a random position and a slightly random angle 
     // on spawn they start a coroutine that moves them in a set direction at a slightly random speed and death timer and random color?
--- Assets/StarManager.cs
+++ Assets/StarManager.cs
@@ -8,6 +8,7 @@ public class StarManager : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject Star;
+    public GameObject SpawnArea;
 
     private Vector3 a = new Vector3(0, 250, 0);
     void Start()
@@ -20,7 +21,21 @@ public class StarManager : MonoBehaviour
     {
         if (Keyboard.current.quoteKey.wasPressedThisFrame)
         {
-           
+
+            Debug.Log("Spawn star");
+
+            float x = Random.Range(100.0f, 150.0f);
+            float y = Random.Range(20.0f, 40.0f);
+            float z = Random.Range(100.0f, 150.0f);
+
+            float yRot = Random.Range(-40f, 40f);
+            Quaternion starDirection = new Quaternion(0, yRot, 0, 0);
+
+            float timeToLive = Random.Range(1.5f, 2.0f);
+
+            float speed = Random.Range(1.0f, 2.0f);
+
+            Instantiate(Star, a, Quaternion.identity);
         }
     }
 }
